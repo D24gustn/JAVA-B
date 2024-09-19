e255b4bf2c7f0af960d4e486cb5e1cb8671b626f
D24gustn <162386334+D24gustn@users.noreply.github.com>
2024-09-19 19:57:27 +0900

    Add files via upload

---
+++ ReStart01/JavaProgramming01.java
@@ -0,0 +1,16 @@
+package ReStart01;
+
+import java.util.Scanner;
+
+public class JavaProgramming01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println("직사각형의 넓이는 " + a*b + "입니다" );
+	}
+	
+}
